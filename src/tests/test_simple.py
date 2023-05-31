919f257cc51d52366295f8b2707a888d0ca82f7f
Bryan He <B.Y.He@student.tudelft.nl>
2023-05-31 09:45:01 +0200

    add pytest fixtures and nondeterminism robustness test

--- src/tests/test_simple.py
+++ src/tests/test_simple.py
@@ -2,16 +2,38 @@
 import pytest
 from .. import train
 from .. import test
+from .. import preprocess
 
+@pytest.fixture()
+def var_smoothing1():
+    var_smoothing1 = 1.1e-09    
+    yield var_smoothing1
 
-def test_model_diff():
-    var_smoothing1 = 1.1e-09
-    var_smoothing2 = 1.2e-09
-    train.test_train(var_smoothing1)
+@pytest.fixture()
+def smoothing1_None_randomstate_score(var_smoothing1):
+    # var_smoothing1 = 1.1e-09    
+    train.test_train(var_smoothing1, None)
     score1 = test.test()
-    train.test_train(var_smoothing2)
+    # baseline score
+    yield score1
+    
+
+def test_model_diff(smoothing1_None_randomstate_score):
+    # var_smoothing1 = 1.1e-09
+    var_smoothing2 = 1.2e-09
+    train.test_train(var_smoothing2, None)
     score2 = test.test()
-    accuracy_diff = abs(score1 - score2)
+    accuracy_diff = abs(smoothing1_None_randomstate_score - score2)
     assert (
         accuracy_diff < 0.1
     ), f"Difference in model accuracy with different seeds is too high: {accuracy_diff}"
+
+def test_nondeterminism_robustness(smoothing1_None_randomstate_score, var_smoothing1):
+    for seed in [1, 2, 3, 4, 5, 42]:
+        preprocess.test_preprocess(random_state=seed)
+        train.test_train(var_smoothing1, seed)
+        score42 = test.test(random_state=seed)
+        accuracy_diff = abs(smoothing1_None_randomstate_score - score42)
+        assert (
+            accuracy_diff < 0.1
+        ), f"Difference in model accuracy with different seeds is too high: {accuracy_diff}"
