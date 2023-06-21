82cbe9551b23c8b3ee6cfb57d2b9356ac32e2cbf
ltroin <123201298@qq.com>
2023-06-21 13:11:18 +0200

    this version every section very good

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -36,7 +36,7 @@ def preprocess_review(review):
     return review
 
 
-def preprocess_data(filename, random_state=41):
+def preprocess_data(filename, random_state=None):
     """
     Preprocesses data, applies bag of words model, and splits data for model training and testing.
 
