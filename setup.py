89c4025f078397812ae67afbcd22e127061e64d7
Nektarios Evangelou <nektarios@evangelou.nl>
2023-06-27 15:57:50 +0200

    Correcting description and author in setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Relma Course ',
-    author='Relma23-Team12',
+    description='REMLA Course ',
+    author='remla23-team12',
     license='MIT',
 )
